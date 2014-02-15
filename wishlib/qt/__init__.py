3465856a6e2ed90e4f89ca4c63a2573e7cf5bf32
csaez <cesarte@gmail.com>
2014-02-15 16:23:55 -0300

    BUGFIX: decorators

--- wishlib/qt/__init__.py
+++ wishlib/qt/__init__.py
@@ -2,6 +2,7 @@ import sys
 
 from . import binding
 from . import widgets
+from . import decorators
 from .helpers import set_style, set_icon, loadUi, wrapinstance
 
 # monkey patch helpers
@@ -10,6 +11,7 @@ binding.set_icon = set_icon
 binding.loadUi = loadUi
 binding.wrapinstance = wrapinstance
 binding.widgets = widgets  # customized qt widgets
+binding.decorators = decorators
 
 ref = sys.modules[__name__]
 sys.modules[__name__] = binding
