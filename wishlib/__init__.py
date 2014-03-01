0dc150f9ceb3b9f4c520d5fc142a5fe71334d127
csaez <cesarte@gmail.com>
2014-03-01 19:48:11 -0300

    sianchor unified

--- wishlib/__init__.py
+++ wishlib/__init__.py
@@ -1,3 +1,9 @@
 from . import utils
 from .si import inside_softimage
 from .ma import inside_maya
+
+if inside_softimage():
+    from .si import show_qt, sianchor
+    anchor = sianchor
+elif inside_maya():
+    from .ma import show_qt, anchor
